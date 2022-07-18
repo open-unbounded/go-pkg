915705d6bff4e888c790d619af4587eb88a9a0d8
chenquan <chenquan.dev@gmail.com>
2022-07-18 13:14:41 +0800

    feat(errs): add NotNil function in AtomicErrs

--- errs/atomic_errs.go
+++ errs/atomic_errs.go
@@ -5,17 +5,26 @@ import (
 	"sync"
 )
 
+// AtomicErrs an error that can hold multiple errors.
 type AtomicErrs struct {
 	rw   sync.RWMutex
 	errs []error
 }
 
-func (a *AtomicErrs) Add(err error) {
+// Add adds errs to be, nil errors are ignored.
+func (a *AtomicErrs) Add(errs ...error) {
 	a.rw.Lock()
-	a.errs = append(a.errs, err)
+	for _, err := range errs {
+		if err == nil {
+			continue
+		}
+
+		a.errs = append(a.errs, err)
+	}
 	a.rw.Unlock()
 }
 
+// Err returns an error that represents all errors.
 func (a *AtomicErrs) Error() string {
 	a.rw.RLock()
 	defer a.rw.RUnlock()
@@ -31,4 +40,9 @@ func (a *AtomicErrs) Error() string {
 	}
 
 	return builder.String()
+}
+
+// NotNil checks if any error inside.
+func (a *AtomicErrs) NotNil() bool {
+	return len(a.errs) > 0
 }
