9a330c05a2cd17a2ca7436ffe18bea183ea824df
chen quan <chenquan.dev@gmail.com>
2022-07-21 14:30:58 +0800

    feat(task): add ScheduledTask (#8)
    * feat(task): add ScheduledTask
    
    * feat(task): add comments

---
+++ task/scheduled_task.go
@@ -0,0 +1,66 @@
+package task
+
+import (
+	"fmt"
+	"time"
+
+	syncx "github.com/open-unbounded/go-pkg/sync"
+	"go.uber.org/atomic"
+)
+
+// ScheduledTask represents a scheduled task
+type ScheduledTask struct {
+	do          func()
+	duration    *atomic.Duration
+	startDoOnce func()
+	stopDoOnce  func()
+	stopChan    chan struct{}
+}
+
+// NewScheduledTask returns a ScheduledTask.
+func NewScheduledTask(do func(), duration time.Duration) *ScheduledTask {
+	s := &ScheduledTask{do: do, duration: atomic.NewDuration(duration), stopChan: make(chan struct{})}
+	s.startDoOnce = syncx.DoOnce(s.doStart)
+	s.stopDoOnce = syncx.DoOnce(s.doStop)
+
+	return s
+}
+
+// Start starts scheduling tasks.
+func (s *ScheduledTask) Start() {
+	s.startDoOnce()
+}
+
+func (s *ScheduledTask) doStart() {
+	go func() {
+		s.do()
+		ticker := time.NewTicker(s.duration.Load())
+		for {
+			<-ticker.C
+			s.do()
+
+			select {
+			case <-s.stopChan:
+				fmt.Println("退出")
+				return
+			default:
+			}
+			ticker.Reset(s.duration.Load())
+
+		}
+	}()
+}
+
+// Stop gracefully stop a scheduled task.
+func (s *ScheduledTask) Stop() {
+	s.stopDoOnce()
+}
+
+func (s *ScheduledTask) doStop() {
+	close(s.stopChan)
+}
+
+// SetDuration sets the interval for scheduling tasks
+func (s ScheduledTask) SetDuration(duration time.Duration) {
+	s.duration.Store(duration)
+}
---
+++ task/scheduled_task_test.go
@@ -0,0 +1,25 @@
+package task
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/atomic"
+)
+
+func TestScheduledTask(t *testing.T) {
+	i := &atomic.Int32{}
+	task := NewScheduledTask(func() {
+		i.Add(1)
+		fmt.Println(i.Load())
+	}, time.Second)
+	task.Start()
+	defer task.Stop()
+	time.Sleep(time.Second + time.Second/2)
+	task.Stop()
+
+	time.Sleep(time.Second * 2)
+	assert.EqualValues(t, 3, i.Load())
+}
