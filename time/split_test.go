8fae0128303b9f5480d29b7b159d7fd542a2c47a
chen quan <chenquan.dev@gmail.com>
2022-07-28 11:08:15 +0800

    feat(time): add Split function (#18)
    * feat(time): add Split function
    
    * feat(time): add comments
    
    * feat(time): reformat code

---
+++ time/split_test.go
@@ -0,0 +1,52 @@
+package time
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplit(t *testing.T) {
+	start := time.Now()
+	end := start.Add(time.Hour - time.Second*40)
+	assert.Len(t, Split(start, end, time.Minute), 60)
+
+	start = time.Now()
+	end = start.Add(time.Hour + time.Second*40)
+	assert.Len(t, Split(start, end, time.Minute), 61)
+
+	start = time.Now()
+	end = start.Add(time.Hour)
+	assert.Len(t, Split(start, end, time.Minute), 60)
+
+	// ------
+
+	start = time.Now()
+	end = start.Add(time.Hour - time.Second*40)
+	assert.Len(t, Split(start, end, time.Second), 60*60-40)
+
+	start = time.Now()
+	end = start.Add(time.Hour + time.Second*40)
+	assert.Len(t, Split(start, end, time.Second), 60*60+40)
+
+	start = time.Now()
+	end = start.Add(time.Hour)
+	assert.Len(t, Split(start, end, time.Second), 60*60)
+
+	// ---
+
+	start = time.Now()
+	end = start
+	assert.Len(t, Split(start, end, time.Minute), 0)
+
+	start = time.Now()
+	end = start.Add(time.Hour)
+	assert.Len(t, Split(start, end, 0), 0)
+
+	// ---
+
+	start = time.Now()
+	end = start.Add(time.Hour)
+	assert.Len(t, Split(start, end, time.Hour), 1)
+}
