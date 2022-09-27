75c63d046f10cbfe62405109cb62d0f667bfe553
chen quan <chenquan.dev@gmail.com>
2022-09-27 11:23:57 +0800

    feat(strs): add Truncate function (#26)
    * feat(strs): add Truncate function
    
    * Update strs/strs_test.go
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- strs/strs.go
+++ strs/strs.go
@@ -28,3 +28,39 @@ func Underscore(s string) string {
 
 	return builder.String()
 }
+
+// Truncate returns  truncated string of the specified length and
+// s consists entirely of valid UTF-8-encoded runes.
+func Truncate(s string, length int) string {
+	if length == 0 {
+		return ""
+	}
+
+	if length < 0 {
+		return s
+	}
+
+	size := len(s)
+	if size <= length {
+		return s
+	}
+
+	var (
+		n, i int
+		c    rune
+	)
+	for i, c = range s {
+		if n == length-1 {
+			break
+		}
+
+		n++
+	}
+
+	i += len(string(c))
+	if i > size {
+		i = size
+	}
+
+	return s[:i]
+}
